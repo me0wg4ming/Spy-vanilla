aa2e359132b58ad6e93d992c4913cfbea7375db1
Meow <30401521+me0wg4ming@users.noreply.github.com>
2025-04-07 17:57:23 +0200

    Add files via upload
    More fixes....

--- Spy.lua
+++ Spy.lua
@@ -2049,15 +2049,14 @@ local function scanName(name)
 end
 
 function Spy:PlayerTargetEvent()
-    local name = GetUnitName("target", true)
-    if name and UnitIsPlayer("target") and UnitIsPVP("target") and not UnitIsFriend("player","target") 
-		and UnitFactionGroup("player") ~= UnitFactionGroup("target") and UnitCanAttack("player", "target")
-			and not SpyPerCharDB.IgnoreData[name] then
-
+	local name = GetUnitName("target", true)
+	if name
+		and UnitIsPlayer("target")	and UnitIsPVP("target")	and not UnitIsFriend("player", "target")
+			and UnitFactionGroup("player") ~= UnitFactionGroup("target")	and UnitCanAttack("player", "target")
+				and not SpyPerCharDB.IgnoreData[name]
+	then
 		local playerData = SpyPerCharDB.PlayerData[name]
-		if UnitIsPVP("target") and not UnitIsFriend("player","target") 
-		and UnitFactionGroup("player") ~= UnitFactionGroup("target") and UnitCanAttack("player", "target")
-		then
+		if UnitIsEnemy("player", "target") then
 			local learnt = true
 			if playerData and playerData.isGuess == false then learnt = false end
 
@@ -2099,14 +2098,14 @@ function Spy:PlayerTargetEvent()
 end
 
 function Spy:PlayerMouseoverEvent()
-    local name = GetUnitName("mouseover", true)
-    if name and UnitIsPlayer("mouseover") and UnitIsPVP("mouseover") and not UnitIsFriend("player","mouseover") 
-		and UnitFactionGroup("player") ~= UnitFactionGroup("mouseover") and UnitCanAttack("player", "mouseover")
-			and not SpyPerCharDB.IgnoreData[name] then
+	local name = GetUnitName("mouseover", true)
+	if name
+		and UnitIsPlayer("mouseover")	and UnitIsPVP("mouseover")	and not UnitIsFriend("player", "mouseover")
+			and UnitFactionGroup("player") ~= UnitFactionGroup("mouseover")	and UnitCanAttack("player", "mouseover")
+				and not SpyPerCharDB.IgnoreData[name]
+	then
 		local playerData = SpyPerCharDB.PlayerData[name]
-		if UnitIsPVP("mouseover") and not UnitIsFriend("player","mouseover") 
-		and UnitFactionGroup("player") ~= UnitFactionGroup("mouseover") and UnitCanAttack("player", "mouseover")
-		then
+		if UnitIsEnemy("player", "mouseover") then
 			local learnt = true
 			if playerData and playerData.isGuess == false then learnt = false end
 
